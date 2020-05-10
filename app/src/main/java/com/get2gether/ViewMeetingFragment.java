f6eb0e5fef72d9215b40663f987e4c22dfc266c0
Grey Ruessler <greyruessler@gmail.com>
2020-05-10 18:17:24 -0500

    recyclerview with accept and decline

--- app/src/main/java/com/get2gether/ViewMeetingFragment.java
+++ app/src/main/java/com/get2gether/ViewMeetingFragment.java
@@ -14,12 +14,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Date;
 
 public class ViewMeetingFragment extends Fragment {
-    private MainActivity parentActivity;
     private ArrayList<Meeting> meetingList;
 
     @Override
@@ -37,10 +38,9 @@ public class ViewMeetingFragment extends Fragment {
 
         Context ctx = getContext();
 
-        parentActivity = (MainActivity) getActivity();
-
         meetingList = new ArrayList<>();
         meetingList.add(new Meeting(new Date(),"Grey Ruessler","Fuck you","05/23/2000 6:00-9:00"));
+        meetingList.add(new Meeting(new Date(),"Garry Russel","Fuck you too","05/23/2000 6:00-9:00"));
 
 
         populateMeetings(ctx, view);
@@ -48,9 +48,35 @@ public class ViewMeetingFragment extends Fragment {
     }
 
     private void populateMeetings(Context ctx, View v) {
-        Activity act = getActivity();
-        ArrayAdapter adapter = new MeetingAdapter(act,0,meetingList);
-        ListView lv = v.findViewById(R.id.meetingListView);
-        lv.setAdapter(adapter);
+        RecyclerView recyclerView = v.findViewById(R.id.meetingListView);
+        MeetingAdapter mAdapter = new MeetingAdapter(meetingList);
+        mAdapter.setAcListener( (acceptButton, position) -> {
+            System.out.print("Accepting: ");
+            System.out.println(meetingList.get(position).getTitle());
+            //TODO: Pipe stuff to web
+            meetingList.remove(position);
+            recyclerView.removeViewAt(position);
+            mAdapter.notifyItemRemoved(position);
+            mAdapter.notifyItemRangeChanged(position, meetingList.size());
+        });
+        mAdapter.setDcListener( (declineButton, position) -> {
+            System.out.print("Declining: ");
+            System.out.println(meetingList.get(position).getTitle());
+            //TODO: Pipe stuff to web
+            meetingList.remove(position);
+            recyclerView.removeViewAt(position);
+            mAdapter.notifyItemRemoved(position);
+            mAdapter.notifyItemRangeChanged(position, meetingList.size());
+        });
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(ctx);
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(mAdapter);
     }
 }
